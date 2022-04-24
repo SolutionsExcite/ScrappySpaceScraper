66eb7720f892f9b3051abac9faab926213ebb70f
SolutionsExcite <8799302+SolutionsExcite@users.noreply.github.com>
2022-04-24 15:39:15 -0500

    Foundation
    created new module navigate.py:
    
    This will handle navigating through all pages on stfc.space.
    
    it is storing the cache storage in a custom profile ScrappySpaceScraper to keep separate from actual browser and easy to find.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from parsel import Selector
-from selenium import webdriver
+from bs4 import BeautifulSoup
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-# from seleniumwire import webdriver
-# from seleniumwire.webdriver.chrome.options import Options
 from pathlib import Path
+import time
+import navigate
 
 # `cwd`: current directory is straightforward (assumes this file location is in root directory)
 project_root = Path.cwd()
@@ -18,48 +17,40 @@ project_root = Path.cwd()
 chrome_driver_location = (project_root / 'drivers/chromedriver.exe').resolve()
 
 if __name__ == '__main__':
-
-    # configure webdriver
-    options = Options()
-    options.headless = True  # hide GUI
-    options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
-    options.add_argument("start-maximized")  # ensure window is full-screen
-
-    # configure chrome browser to not load images and javascript
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option(
-        # this will disable image loading
-        "prefs", {"profile.managed_default_content_settings.images": 2}
-    )
-
-    # Go to home page initially in case it sets session data there
-    space_url = 'https://stfc.space/ships'
-
-    # Create a new instance of the Chrome driver
-    # driver = webdriver.Chrome(executable_path=chrome_driver_location, options=options, chrome_options=chrome_options)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,
-                              chrome_options=chrome_options)
-
-    # stfc.space
-    driver.get(space_url)
-
-    # wait for page to load
-    # waiting for: <span class="truncate font-bold">AMALGAM</span>
-    element = WebDriverWait(driver=driver, timeout=5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'truncate'))
-    )
-    print(driver.page_source)
-
-    # Access requests via the `requests` attribute
-    # for request in driver.requests:
-    #     if request.response:
-    #         print(
-    #             request.url,
-    #             request.response.status_code,
-    #             request.response.headers['Content-Type']
-    #         )
-
-
-
-
-
+    navigate.navigate_site()
+    # # configure webdriver
+    # options = Options()
+    # options.headless = True  # hide GUI
+    # options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
+    # options.add_argument("start-maximized")  # ensure window is full-screen
+#
+    # # configure chrome browser to not load images and javascript
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_experimental_option(
+    #     # this will disable image loading
+    #     "prefs", {"profile.managed_default_content_settings.images": 2}
+    # )
+    # # options.add_argument("user-data-dir=C:/Users/lenha/AppData/Local/Google/Chrome/User Data/Default")
+    # chrome_options.add_argument("user-data-dir=C:/Users/lenha/AppData/Local/Google/Chrome/"
+    #                             "User Data/ScrappySpaceScraper")
+#
+    # # Go to home page initially in case it sets session data there
+    # space_url = 'https://stfc.space/ships?f=$name=%26faction:-1%26igrade:-1%26irarity:-1%26' \
+    #             'page:1' \
+    #             '&s=$ascending:true%26sortBy:0'
+#
+    # # Create a new instance of the Chrome driver
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,
+    #                           chrome_options=chrome_options)
+#
+    # # stfc.space
+    # driver.get(space_url)
+#
+    # # wait for page to load
+    # # waiting for: <span class="truncate font-bold">AMALGAM</span>
+    # element = WebDriverWait(driver=driver, timeout=5).until(
+    #     EC.presence_of_element_located((By.CLASS_NAME, 'truncate.font-bold'))
+    # )
+    # time.sleep(3000)
+    # soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # print(soup.prettify())
