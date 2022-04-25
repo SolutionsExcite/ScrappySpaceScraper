0d41d130dd45e4eac80b99da4ae656eb78094ff1
SolutionsExcite <8799302+SolutionsExcite@users.noreply.github.com>
2022-04-25 01:31:53 -0500

    Refactoring
    took ship section loading and made it generic so that we can reuse it for all section. Leaving current past code in on development for now.

--- navigate.py
+++ navigate.py
@@ -68,8 +68,99 @@ def navigate_ships(driver):
             navigate_ship(driver, ship_tag['href'][1:])
 
 
+def navigate_officers(driver):
+    # Base ships url to loop through pages
+    officer_query_string = '/officers?f=$name=%26sg:-1%26rarity:-1%26' \
+                           'page:{current_page}&s=$ascending:true%26sortBy:0'
+    officers_url = stfc_base_url + officer_query_string.format(current_page=1)
+
+    # CSS selector of element with data-attr whose value is async loaded
+    css_loaded_selector = 'a[href^="/officers/"] '
+
+    # get soup, so we start paging and grabbing all the ship ID's
+    soup = get_soup(driver, officers_url, css_loaded_selector)
+
+    # Get visible number of pages
+    css_pages_selector = 'nav a.bg-white'
+    page_tags = soup.select(css_pages_selector)
+
+    # Map to get string. Filter for only numbers.  Place in set to ensure unique values
+    pages = list(set(map(lambda x: x.string, page_tags)))
+    pages.remove(None)
+    pages.sort()
+
+    # Loop over each page scanning all ships on page
+    for page in pages:
+        if page != 1:
+            officers_url = stfc_base_url + officer_query_string.format(current_page=page)
+            # load page
+            soup = get_soup(driver, officers_url, css_loaded_selector)
+
+        # Grab anchor with href storing specific ship url
+        officer_tags = soup.select(css_loaded_selector)
+        for ship_tag in officer_tags:
+            # select value of href (string). [1:] takes the string and returns characters skipping first character
+            navigate_ship(driver, ship_tag['href'][1:])
+
+
+def get_navigation_pages(soup):
+    # Get visible number of pages
+    css_pages_selector = 'nav a.bg-white'
+    page_tags = soup.select(css_pages_selector)
+
+    # Map to get string. Place in set to ensure unique values
+    pages = list(set(map(lambda x: x.string, page_tags)))
+    pages.remove(None)  # This removes anchors that are navigation arrows. they have nothing in tag.string
+
+    # Create a list from 1...max
+    max_val = int(max(pages))
+    pages = [i for i in range(1, max_val + 1)]
+
+    return pages
+
+
+def navigate_section_page_item(driver, item_id, css_item_loaded_selector):
+    item_url = stfc_base_url + item_id
+
+    # load unique item page.
+    soup = get_soup(driver, item_url, css_item_loaded_selector)
+    return soup
+
+
+def navigate_section_pages(driver, query_string, css_page_loaded_selector, css_item_loaded_selector):
+    # First page in section of url to loop through pages
+    section_url = stfc_base_url + query_string.format(current_page=1)
+
+    # get soup, so we start paging and grabbing all the ship ID's
+    soup = get_soup(driver, section_url, css_page_loaded_selector)
+
+    # Get a list of pages from 1...n
+    pages = get_navigation_pages(soup)
+
+    # Loop over each page scanning all ships on page
+    for page in pages:
+        if page != 1:
+            section_url = stfc_base_url + query_string.format(current_page=page)
+            # load page
+            soup = get_soup(driver, section_url, css_page_loaded_selector)
+
+        # Grab anchor with href storing specific ship url
+        item_tags = soup.select(css_page_loaded_selector)
+        for item_tag in item_tags:
+            # select value of href (string). [1:] takes the string and returns characters skipping first character
+            navigate_section_page_item(driver, item_tag['href'][1:], css_item_loaded_selector)
+
+
 def navigate_sections(driver):
-    navigate_ships(driver)
+    # Navigate ships section
+    query_string = 'ships?f=$name=%26faction:-1%26igrade:-1%26irarity:-1%26' \
+                        'page:{current_page}&s=$ascending:true%26sortBy:0'
+    # CSS selector of element with data-attr whose value is async loaded for page
+    css_page_loaded_selector = 'a[href^="/ships/"] '
+    # CSS selector of element with classes
+    css_item_loaded_selector = 'td.text-center.px-2.py-2.text-sm.tabular-nums'
+    navigate_section_pages(driver, query_string, css_page_loaded_selector, css_item_loaded_selector)
+    # navigate_ships(driver)
 
 
 def navigate_site():
@@ -85,7 +176,6 @@ def navigate_site():
         # this will disable image loading
         "prefs", {"profile.managed_default_content_settings.images": 2}
     )
-    # options.add_argument("user-data-dir=C:/Users/lenha/AppData/Local/Google/Chrome/User Data/Default")
     chrome_options.add_argument("user-data-dir=C:/Users/lenha/AppData/Local/Google/Chrome/"
                                 "User Data/ScrappySpaceScraper")
 
